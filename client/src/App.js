a2911cf6a24f7fec9984ad2cf1258d2866f708f3
Ankush Challa <ankushchalla@gmail.com>
2021-08-20 21:52:12 -0700

    Cleaned up useAuth hook.

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,31 @@
 import './App.css';
 
-import React, { useState, useEffect} from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from 'react-router-dom'
-import axios from 'axios';
+import React from 'react';
 import { useAuth } from './useAuth';
 
 export default function App() {
-  useAuth();
+  const isValidUser = useAuth();
   // Make a post request to server w/ proper credentials.
 
   // Cookie header gets automatically set.
   // Make GET request. If successful, display reponse. 
+  return isValidUser ? <AuthorizedApp /> : <UnauthorizedApp />
+}
+
+function AuthorizedApp() {
   return (
-    <></>
-  );
+    <>
+    <h1>User authorized</h1>
+    </>
+  )
+}
+
+function UnauthorizedApp() {
+  return (
+    <>
+    <h1>User not authorized</h1>
+    </>
+  )
 }
 
 
--- client/src/useAuth.js
+++ client/src/useAuth.js
@@ -1,23 +1,19 @@
-import React, { useState, useEffect} from 'react';
+import { useState, useEffect} from 'react';
 import axios from 'axios';
-// what does it do?
 export function useAuth() {
     const [isValidUser, setIsValidUser] = useState(false)
-    useEffect(async () => {
-        const loginResponse = await login()
-        isValidUser = loginResponse.status === 200
-    })
+    useEffect(() => {
+        function fetchLoginStatus() {
+            axios.post('/login', {
+                email: "ankush@gmail.com", 
+                password: "secret"
+              })
+              .then(() => setIsValidUser(true))
+              .catch(() => setIsValidUser(false))
+        }
+        fetchLoginStatus()
+    }, [])
     return isValidUser
-}
-
-function login() {
-    return new Promise(resolve => {
-        axios.post('/login', {
-            email: "ankush@gmail.com", 
-            password: "secret"
-          })
-          .then(resolve)
-    })
 }
 
 

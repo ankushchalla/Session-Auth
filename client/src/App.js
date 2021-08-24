f7fd82b6421c207107af5f759ff9d06f890413a5
Ankush Challa <ankushchalla@gmail.com>
2021-08-24 09:56:24 -0700

    Added routes with React Router.

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,12 @@
 import './App.css';
 
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import { useAuth } from './useAuth';
 
 export default function App() {
@@ -15,7 +21,36 @@ export default function App() {
 function AuthorizedApp() {
   return (
     <>
-    <h1>User authorized</h1>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/users">Users</Link>
+              </li>
+            </ul>
+          </nav>
+          {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+          <Switch>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/users">
+              <Users />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </>
   )
 }
@@ -23,9 +58,21 @@ function AuthorizedApp() {
 function UnauthorizedApp() {
   return (
     <>
-    <h1>User not authorized</h1>
+      <h1>User not authorized</h1>
     </>
   )
+}
+
+function Home() {
+  return <h2>Authorized user in home.</h2>;
+}
+
+function About() {
+  return <h2>Authorized user in about.</h2>;
+}
+
+function Users() {
+  return <h2>Authorized user in users.</h2>;
 }
 
 
--- client/src/useAuth.js
+++ client/src/useAuth.js
@@ -1,17 +1,15 @@
-import { useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 export function useAuth() {
     const [isValidUser, setIsValidUser] = useState(false)
     useEffect(() => {
-        function fetchLoginStatus() {
-            axios.post('/login', {
-                email: "ankush@gmail.com", 
-                password: "secret"
-              })
-              .then(() => setIsValidUser(true))
-              .catch(() => setIsValidUser(false))
-        }
-        fetchLoginStatus()
+        axios.post('/login', {
+            email: "ankush@gmail.com",
+            password: "secrt"
+        })
+        .then(() => setIsValidUser(true))
+        .catch(() => setIsValidUser(false))
+
     }, [])
     return isValidUser
 }
